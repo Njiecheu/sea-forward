7302e27180d712ce9177d4f4fb62b26d30fe9c33
Njiecheu <raphaelnjiecheu@gmail.com>
2026-08-01 12:17:10 +0100

    Bloc de code par défaut

--- docs/javascripts/copy-button.js
+++ docs/javascripts/copy-button.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   var copyIcon = `
-  <svg viewBox="0 0 24 24" width="15" height="15" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <rect x="8" y="8" width="12" height="12" rx="2"></rect>
-    <path d="M4 16V4a2 2 0 0 1 2-2h10"></path>
-  </svg>`;
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <rect x="9" y="9" width="13" height="13" rx="2"></rect>
+      <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+    </svg>`;
 
   var checkIcon = `
-    <svg viewBox="0 0 24 24" width="15" height="15" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <polyline points="20 6 9 17 4 12"></polyline>
     </svg>`;
 
@@ -32,7 +32,13 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
 
-    block.style.position = "relative";
-    block.appendChild(button);
+    var filenameBar = block.querySelector(".filename");
+    if (filenameBar) {
+      filenameBar.style.position = "relative";
+      filenameBar.appendChild(button);
+    } else {
+      block.style.position = "relative";
+      block.appendChild(button);
+    }
   });
 });
