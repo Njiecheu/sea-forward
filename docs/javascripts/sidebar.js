11a3a360c034120daf89262123a14da9e16d946d
kydlaugan <kaptchouangyvand@gmail.com>
2026-08-01 15:54:10 +0100

    correct side bar

--- docs/javascripts/sidebar.js
+++ docs/javascripts/sidebar.js
@@ -1,27 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Sélectionner tous les éléments de niveau 1 qui ont un sous-menu (ul)
-    var topLevelItems = document.querySelectorAll(".wy-menu-vertical li.toctree-l1");
+    // 1. Gérer les grands titres (Phases) qui sont des <p class="caption">
+    var captions = document.querySelectorAll(".wy-menu-vertical p.caption");
+    captions.forEach(function (caption) {
+        caption.style.cursor = "pointer";
+        caption.classList.add("is-collapsible");
+        
+        var nextUl = caption.nextElementSibling;
+        if (nextUl && nextUl.tagName.toLowerCase() === "ul") {
+            caption.addEventListener("click", function () {
+                // Fermer/ouvrir uniquement cette section
+                caption.classList.toggle("js-open");
+                nextUl.classList.toggle("js-open");
+            });
+        }
+    });
 
+    // 2. Gérer les sous-sections (liens avec enfants) de type toctree-l1 ou l2
+    var topLevelItems = document.querySelectorAll(".wy-menu-vertical li.toctree-l1, .wy-menu-vertical li.toctree-l2");
     topLevelItems.forEach(function (li) {
         var subMenu = li.querySelector("ul");
         if (subMenu) {
-            // Ajouter une classe pour identifier qu'il a des enfants
             li.classList.add("has-children");
-
-            // Trouver le lien ou le label cliquable
-            var link = li.querySelector("a") || li.querySelector(".caption");
-            
+            var link = li.querySelector("a");
             if (link) {
                 link.style.cursor = "pointer";
-                
                 link.addEventListener("click", function (e) {
-                    // Si c'est juste un label ou si on veut forcer l'ouverture sans changer de page
-                    // On bascule l'état ouvert/fermé
-                    li.classList.toggle("js-open");
-                    
-                    // Si le lien a un href "#" ou s'il s'agit d'un label sans lien réel, empêcher la navigation
+                    // Empêcher la navigation si c'est juste un lien vide (#)
                     if (!link.getAttribute("href") || link.getAttribute("href") === "#") {
                         e.preventDefault();
+                        li.classList.toggle("js-open");
+                    } else {
+                        // Si c'est un vrai lien, on peut l'ouvrir visuellement aussi
+                        li.classList.toggle("js-open");
                     }
                 });
             }
